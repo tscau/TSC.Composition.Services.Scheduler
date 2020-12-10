920c81a8d841c1811acd8c327096a82063384978
Richard Angus <richard.angus@live.com.au>
2020-12-10 21:11:31 +1000

    Added Startup class

--- TSC.Composition.Services.Scheduler/Startup.cs
+++ TSC.Composition.Services.Scheduler/Startup.cs
@@ -1,10 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Hangfire;
+using Hangfire.JobsLogger;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+using TSC.Composition.Services.Scheduler.Config;
 
 namespace TSC.Composition.Services.Scheduler
 {
-    class Startup
+    public class Startup
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var appConfig = new AppConfig();
+
+            services.AddControllers();
+            services.ConfigureSwaggerGen(c => c.CustomSchemaIds(type => type.FullName));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Scheduler API",
+                    Version = "v1",
+                    Description = "The Scheduler API used to manage the Scheduler via a REST endpoint"
+                    
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+
+
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSerilogLogProvider()
+                .UseJobsLogger()
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(appConfig.ConnectionStrings.HangfireCompositionDb, new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    UsePageLocksOnDequeue = true,
+                    DisableGlobalLocks = true,
+                    PrepareSchemaIfNecessary = true
+                }));
+            //var queues = new string[] { GlobalSchedulerQueues.GlobalSchedulerPutQueue };
+            var queues = new string[] { "Default" };
+            services.AddHangfireServer(options =>
+            {
+                options.Queues = queues;
+                options.WorkerCount = appConfig.AppSettings.HangfireWorkerCount;
+            });
+
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Scheduler API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
     }
 }
